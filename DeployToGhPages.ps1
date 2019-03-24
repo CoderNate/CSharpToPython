6ae58799cd34781517f1c08c3b75c74af637c466
CoderNate <ncdahlquist@yahoo.com>
2019-03-24 15:02:26 -0500

    Add --quiet option to git clone and push
    Prevents git from writing to StdErr and making Azure pipelines think that errors are happening

--- DeployToGhPages.ps1
+++ DeployToGhPages.ps1
@@ -16,11 +16,11 @@ if (Test-Path tempDir) {
 }
 Write-Host "About to clone into $(Join-Path (Resolve-Path .).Path -ChildPath tempDir)"
 # Have to clone into a new directory or git will error saying the directory already exists and is not empty
-ExecSafe {git clone --no-checkout --branch=gh-pages "https://$($githubusername):$githubaccesstoken@github.com/$githubusername/CSharpToPython.git" tempDir}
+ExecSafe {git clone --no-checkout --branch=gh-pages "https://$($githubusername):$githubaccesstoken@github.com/$githubusername/CSharpToPython.git" tempDir --quiet}
 Move-Item tempDir\.git .git -force
 git config core.autocrlf false
 git config user.email ncdahlquist@yahoo.com
 git config user.name $githubusername
 git add -A
 git commit --amend --no-edit
-ExecSafe {git push --force-with-lease}
+ExecSafe {git push --force-with-lease --quiet}
