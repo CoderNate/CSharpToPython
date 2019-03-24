bf31b957ca271e29d73886a6fcb9c4cfec4a9a44
CoderNate <ncdahlquist@yahoo.com>
2019-03-24 14:49:05 -0500

    Minor changes to index.html

--- DeployToGhPages.ps1
+++ DeployToGhPages.ps1
@@ -4,15 +4,23 @@ param (
 )
 
 $ErrorActionPreference = "Stop"
+function ExecSafe([scriptblock] $cmd) {
+    & $cmd
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
 
 $githubusername = "CoderNate"
 cd $artifactDir
+if (Test-Path tempDir) {
+	throw "TempDIr already exists"
+}
+Write-Host "About to clone into $(Join-Path (Resolve-Path .).Path -ChildPath tempDir)"
 # Have to clone into a new directory or git will error saying the directory already exists and is not empty
-git clone --no-checkout --branch=gh-pages "https://$($githubusername):$githubaccesstoken@github.com/$githubusername/CSharpToPython.git" tempDir
-Move-Item tempDir\.git .\.git -force
+ExecSafe {git clone --no-checkout --branch=gh-pages "https://$($githubusername):$githubaccesstoken@github.com/$githubusername/CSharpToPython.git" tempDir}
+Move-Item tempDir\.git .git -force
 git config core.autocrlf false
 git config user.email ncdahlquist@yahoo.com
 git config user.name $githubusername
 git add -A
 git commit --amend --no-edit
-git push --force-with-lease
+ExecSafe {git push --force-with-lease}
